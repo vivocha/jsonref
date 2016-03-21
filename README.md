1bf0c57cfb5a0d7152325d67591de0e0b41ea07a
Federico Pinna <fpinna@vivocha.com>
2016-03-21 18:03:12 +0100

    First official release.

--- README.md
+++ README.md
@@ -1,1 +1,190 @@
 # jsonref
+
+A simple Javascript library implementing [JSON Reference](http://tools.ietf.org/html/draft-pbryan-zyp-ref-03) and [JSON Pointer](http://tools.ietf.org/html/rfc6901) specifications.
+
+[![Build Status](https://travis-ci.org/vivocha/jsonref.svg?branch=master)](https://travis-ci.org/vivocha/jsonref)
+[![NPM version](https://badge.fury.io/js/jsonref.png)](http://badge.fury.io/js/jsonref)
+
+[![Dependency Status](https://david-dm.org/vivocha/jsonref/status.svg)](https://david-dm.org/vivocha/jsonref)
+[![devDependency Status](https://david-dm.org/vivocha/jsonref/dev-status.svg)](https://david-dm.org/vivocha/jsonref#info=devDependencies)
+
+## Install
+
+```bash
+$ npm install jsonref
+```
+
+## parse(dataOrUri, store, retriever)
+
+* `dataOrUri`, the data to parse or a fully qualified URI to pass to `retriever` to download the data
+* `store` (optional), an object to use to cache resolved `id`  and `$ref` values. If no store is passed,
+one is automatically created. Pass a `store` if you are going to parse several objects or URIs referencing
+the same `id` and `$ref` values.
+* `retriever` (optional), a function accepting a URL in input and returning a promise resolved to an object
+representing the data downloaded for the URI. Whenever a `$ref` to a new URI is found, if the URI is not
+already cached in the store in use, it'll be fetched using this `retriever`. If not `retriever` is passed
+and a URI needs to be downloaded, a `no_retriever` exception is thrown.
+
+The function returns a Promise resolving to the parsed data, with all `$ref` instances resolved.
+
+### Sample browser-friendly `retriever` using `fetch`
+
+```javascript
+function retriever(url) {
+  var opts = {
+    method: 'GET',
+    credentials: 'include'
+  };
+  return fetch(url, opts).then(function(response) {
+    return response.json();
+  });
+}
+```
+
+### Sample node.js `retriever` using `request`
+
+```javascript
+var request = require('request');
+
+function retriever(url) {
+  return new Promise(function(resolve, reject) {
+    request({
+      url: url,
+      method: 'GET',
+      json: true
+    }, function(err, response, data) {
+      if (err) {
+        reject(err);
+      } else if (response.statusCode !== 200) {
+        reject(response.statusCode);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+```
+
+## pointer(data, path)
+
+* `data`, the object to transverse using JSON Pointer.
+* `path`, either a string (`#/prop1/prop2`) or an array of path components (`[ "#", "prop1", "prop2" ]`
+or `[ "prop1", "prop2" ]`).
+
+Returns the data requested
+
+## Examples
+
+### Parsing an object with no external references
+
+````javascript
+parse({
+  "id": "http://my.site/myschema#",
+  "definitions": {
+    "schema1": {
+      "id": "schema1",
+      "type": "integer"
+    },
+    "schema2": {
+      "type": "array",
+      "items": { "$ref": "schema1" }
+    }
+  }
+}).then(function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+````
+
+The output is:
+
+```javascript
+{
+  "id": "http://my.site/myschema#",
+  "definitions": {
+    "schema1": {
+      "id": "schema1",
+      "type": "integer"
+    },
+    "schema2": {
+      "type": "array",
+      "items": {
+        "id": "schema1",
+        "type": "integer"
+      }
+    }
+  }
+}
+```
+
+### Parsing an object with external references
+
+```javascript
+parse({
+  "allOf": [
+    { "$ref": "http://json-schema.org/draft-04/schema#" },
+    {
+      "type": "object",
+      "properties": {
+        "documentation": {
+          "type": "string"
+        }
+      }
+    }
+  ]
+}, null, retriever).then(function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+```
+
+The library will call `retriever("http://json-schema.org/draft-04/schema#")` to download the external
+reference. If no `retriever` is passed, the returned value is a rejected Promise, with a `no_retriever`
+exception.
+
+### Parsing an object using a custom store
+
+```javascript
+var store = {};
+
+parse({
+  "id": "http://my.site/myschema#",
+  "definitions": {
+    "schema1": {
+      "id": "schema1",
+      "type": "integer"
+    },
+    "schema2": {
+      "type": "array",
+      "items": { "$ref": "schema1" }
+    }
+  }
+}, store).then(function(data) {
+  console.log(JSON.stringify(store, null, 2));
+});
+```
+
+After parsing, the contents of the store are:
+
+```javascript
+{
+  "http://my.site/myschema#": {
+    "id": "http://my.site/myschema#",
+    "definitions": {
+      "schema1": {
+        "id": "schema1",
+        "type": "integer"
+      },
+      "schema2": {
+        "type": "array",
+        "items": {
+          "id": "schema1",
+          "type": "integer"
+        }
+      }
+    }
+  },
+  "http://my.site/schema1#": {
+    "id": "schema1",
+    "type": "integer"
+  }
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import url from 'url';
 
+var __scope = Symbol();
+
 export function pointer(data, path) {
   var _data = data;
   var _path = typeof path === 'string' ? path.split('/') : path;
@@ -12,12 +14,15 @@ export function pointer(data, path) {
   }
   return _data;
 }
-export function parse(data, store, retriever) {
+export function scope(data) {
+  return typeof data === 'object' ? data[__scope] : undefined;
+}
+export function parse(dataOrUri, store, retriever) {
   var _store = store || {};
   var _retriever = retriever || function () {
-    return Promise.reject(new Error('no_retriever'));
-  };
-  var _root = data;
+      return Promise.reject(new Error('no_retriever'));
+    };
+  var _root;
   function _resolve(path, scope) {
     var resolvedPath = url.resolve(scope || '', path || '');
     var parsedPath = url.parse(resolvedPath);
@@ -73,18 +78,20 @@ export function parse(data, store, retriever) {
     return undefined;
   }
   function _parse(path, scope, data) {
+    _root = data;
     function _parsePassOne(data, scope) {
       if (typeof data === 'object') {
         var _scope, i, o;
         if (typeof data.id === 'string') {
           _scope = _register(data.id, scope, data);
         } else {
-          _scope = scope;
+          _scope = scope || '#';
         }
+        data[__scope] = _scope;
         for (i in data) {
           o = data[i];
           if (typeof o === 'object' && !o.$ref) {
-            _parsePassOne(o, _scope);
+            _parsePassOne(o, _scope + '/' + i);
           }
         }
       }
@@ -97,7 +104,7 @@ export function parse(data, store, retriever) {
           var uri = _resolve(data.id, scope);
           _scope = uri.url + uri.hash.join('/');
         } else {
-          _scope = scope;
+          _scope = scope || '#';
         }
         function _deref(key, ref) {
           return p.then(function() {
@@ -109,7 +116,7 @@ export function parse(data, store, retriever) {
         }
         function _recurse(key, obj) {
           return p.then(function() {
-            return _parsePassTwo(obj, _scope);
+            return _parsePassTwo(obj, _scope + '/' + i);
           });
         }
         for (i in data) {
@@ -130,151 +137,10 @@ export function parse(data, store, retriever) {
       return data;
     });
   }
-  return _parse(null, null, _root);
-}
 
-/*
-import { Store } from './store';
-export { Store } from './store';
-
-var _globalStore = new Store();
-var _options = {};
-
-function resolvePath(path, scope) {
-  var resolvedPath = url.resolve(scope || '', path || '');
-  var parsedPath = url.parse(resolvedPath);
-  var hash = parsedPath.hash;
-  delete parsedPath.hash;
-  var out = {
-    url: url.format(parsedPath)
-  };
-  if (hash && hash[0] === '#') {
-    out.hash = hash.substr(1).split('/');
+  if (typeof dataOrUri === 'string') {
+    return _get(dataOrUri);
   } else {
-    out.hash = [];
+    return _parse(null, null, dataOrUri);
   }
-  return out;
 }
-
-export function pointer(data, path) {
-  for (var i = 0 ; path && data && i < path.length ; i++) {
-    if (path[i]) data = data[path[i]];
-  }
-  return data;
-}
-export function parse(data, opts) {
-  var _opts = opts || {};
-  if (!_opts.store) _opts.store = new Store();
-  _opts.store.register("", data);
-  function parsePassOne(data, scope) {
-    var p = Promise.resolve(true);
-    if (typeof data === 'object') {
-      let _scope;
-      if (typeof data.id === 'string') {
-        _scope = resolvePath(data.id, scope || '').url;
-        _opts.store.register(_scope, data);
-      } else {
-        _scope = scope;
-      }
-      function recurse(key, obj) {
-        return p.then(function() {
-          return parsePassOne(obj, _scope);
-        });
-      }
-      let i, o;
-      for (i in data) {
-        o = data[i];
-        if (typeof o === 'object' && !o.$ref) {
-          p = recurse(i, o);
-        }
-      }
-    }
-    return p;
-  }
-  function parsePassTwo(data, scope) {
-    var p = Promise.resolve(true);
-    if (typeof data === 'object') {
-      let _scope;
-      if (typeof data.id === 'string') {
-        _scope = resolvePath(data.id, scope || '').url;
-      } else {
-        _scope = scope;
-      }
-      function deref(key, ref) {
-        return p.then(function() {
-          return resolve(ref, _scope, _opts).then(function() {
-            delete data[key];
-            Object.defineProperty(data, key, {
-              get: function() {
-                var resolved = resolvePath(ref, _scope);
-                var data = (_opts.store ? _opts.store.get(resolved.url) : undefined) || _globalStore.get(resolved.url);
-                return data ? pointer(data, resolved.hash) : undefined;
-              },
-              set: function(data) {
-                //noop
-              }
-            });
-            return true;
-          });
-        });
-      }
-      function recurse(key, obj) {
-        return p.then(function() {
-          return parsePassTwo(obj, _scope);
-        });
-      }
-      let i, o;
-      for (i in data) {
-        o = data[i];
-        if (typeof o === 'object') {
-          if (o.$ref) {
-            p = deref(i, o.$ref);
-          } else {
-            p = recurse(i, o);
-          }
-        }
-      }
-    }
-    return p;
-  }
-  return parsePassOne(data).then(function() {
-    return parsePassTwo(data).then(function() {
-      return data;
-    });
-  });
-}
-export function register(url, data) {
-  _globalStore.register(url, data);
-  return data;
-}
-export function retrieve(url, opts) {
-  var _opts = opts || {};
-  return _opts.retriever(url).then(function(data) {
-    return parse(data, _opts).then(function(data) {
-      return (_opts.store || _globalStore).register(url, data);
-    });
-  });
-}
-export function resolve(path, scope, opts) {
-  var _opts = opts || {};
-  var resolved = resolvePath(path, scope);
-  return Promise.resolve((_opts.store ? _opts.store.get(resolved.url) : undefined) || _globalStore.get(resolved.url) || retrieve(resolved.url, _opts)).then(function(data) {
-    return pointer(data, resolved.hash);
-  });
-}
-export function store() {
-  return _globalStore;
-}
-export function option(key, value) {
-  _options[key] = value;
-}
-*/
-
-
-
-
-
-
-
-
-
