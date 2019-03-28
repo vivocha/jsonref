9a763334b380f62f81ec0e2a1ef8bcecc05974c8
Federico Pinna <fpinna@vivocha.com>
2019-03-28 15:48:11 +0100

    fix: export the Registry interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as meta from './meta';
 import { resolve as refResolver } from './ref';
-export { getMeta, isAnnotated, isRef, Meta, normalize, normalizeUri } from './meta';
+
+export { getMeta, isAnnotated, isRef, Meta, normalize, normalizeUri, Registry } from './meta';
 export { resolve as pointer } from './pointer';
 
 export type Retriever = (url: string)=>Promise<any>;
